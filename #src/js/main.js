289a80a8693c91d6b7dbac96abacb25af5d177b9
Stas Kol <newdom24@gmail.com>
2021-05-25 11:44:22 +0300

    fx: оптимизация картинок, кода, слайдера

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,26 +1,48 @@
 // слайдер на главной
-var reviewsSlider = new Swiper(".tophead-slider", {
-  loop: true,
+var headerSlider = new Swiper(".tophead-slider", {
+  // loop: true,
   autoplay: {
-    delay: 5000,
+    //пауза между прокруткой
+    delay: 3000,
+    //закончить на последнем слайде
+    // stopOnLastSlide: false,
+    //отключить после ручного переключения
+    // disableOnInteraction: false,
   },
+  //скорость переключения слайдов
+  speed: 800,
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
     clickable: true,
+    dynamicBullets: true,
   },
+  //эффект перехода слайда
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  //отложенная загрузка:
+  //отключаем презагрузку картинок
+  preloadImages: false,
+  lazy: {
+    loadOnTransitionStart: false,
+    loadPrevNext: false,
+  },
+  // переключение при клике на слайд
+  slideToClickedSlide: true,
   // отключение прокрутки при наведении мыши
-  on: {
-    init() {
-      this.el.addEventListener("mouseenter", () => {
-        this.autoplay.stop();
-      });
+  // on: {
+  //   init() {
+  //     this.el.addEventListener("mouseenter", () => {
+  //       this.autoplay.stop();
+  //     });
 
-      this.el.addEventListener("mouseleave", () => {
-        this.autoplay.start();
-      });
-    },
-  },
+  //     this.el.addEventListener("mouseleave", () => {
+  //       this.autoplay.start();
+  //     });
+  //   },
+  // },
 });
 //-----
